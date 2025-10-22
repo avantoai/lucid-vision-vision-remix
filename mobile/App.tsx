f7cde4abc0cd394cccefa715da60b33164814bab
Alan VanToai <Alan@VanToai.com>
2025-10-22 21:45:02 +0000

    Revert to working code

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AuthProvider } from './src/contexts/AuthContext';
 import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
-  useEffect(() => {
-    // TEMPORARY: Clear all AsyncStorage on app start
-    AsyncStorage.clear().then(() => {
-      console.log('🧹 AsyncStorage cleared completely');
-    });
-  }, []);
-
   return (
     <SafeAreaProvider>
       <AuthProvider>
